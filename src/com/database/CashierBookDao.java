408b1ecd60e38f248f84d7a744eb362e4eb04c2d
benzintosh <benz_suankularb@hotmail.com>
2013-10-29 18:44:54 +0700

    create All DataBase Interface
    create All DataBase Interface
    and fix some DB constructor

---
+++ src/com/database/CashierBookDao.java
@@ -0,0 +1,14 @@
+package com.database;
+
+import com.core.Cashier;
+
+public interface CashierBookDao {
+	public long insert(Cashier cashier);
+	public Cashier findBy(String name);
+	public Cashier deleteBy(String name);
+	public Cashier[] findByContains(String name);
+	public Cashier delete(int id);
+	public Cashier[] findAll();
+	public void close();
+	public Cashier findBy(int id);
+}
