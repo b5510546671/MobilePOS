346e33db25227b010b6b0ce54836610290a9f6d9
supavitk <supavit_knot@yahoo.co.th>
2013-11-20 15:01:08 +0700

    Fix model Itemdesc and Inventory class
    Final change for connect to dao

--- src/com/core/database/Inventory.java
+++ src/com/core/database/Inventory.java
@@ -1,17 +1,85 @@
 package com.core.database;
 
-import android.content.Context;
+import java.util.List;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.core.InventoryLineItem;
+import com.core.Item;
+import com.core.ItemDescription;
 import com.database.InventoryDB;
+import com.database.InventoryLineItemBookDB;
 
 public class Inventory {
-	InventoryDB inventoryDB;
-	 public boolean removeAll(Context con,int[] IDs)
-	 {
-		 inventoryDB = new InventoryDB(con);
-		 inventoryDB.deleteAll(IDs);
-		 inventoryDB.close();
+	private InventoryDB db;
+	
+	public Item getByID(Context context , int id){
+		db = new InventoryDB(context);
+		Item i = db.findByID(id);
+		db.close();
+		return i;
+	}
+	
+	public List<Item> getAllStock(Context con){
+		db = new InventoryDB(con);
+		List<Item> i = db.findByDescriptionID(Item.SALE_STOCK_ID);
+		db.close();
+		return i;
+	}
+	
+	public List<Item> getItemsByInventoryLineItem(Context con,InventoryLineItem inventoryLineItem)
+	{
+		db = new InventoryDB(con);
+		List<Item> i = db.findByInventoryLineItemID(inventoryLineItem.getID());
+		db.close();
+		return i;
+	}
+	
+	public InventoryLineItem addInventoryLineItem(Context con , InventoryLineItem inventoryLineItem){
+		db = new InventoryDB(con);
+		InventoryLineItem i = db.insert(inventoryLineItem);
+		db.close();
+		return i;
+	}
+	
+	public List<Item> getItemsByItemDescription(Context con , ItemDescription itemDescription){
+		db = new InventoryDB(con);
+		List<Item> i = db.findByDescriptionID(itemDescription.getId());
+		db.close();
+		return i;
+	}
+	
+	public InventoryLineItem getInventoryLineItemByID(Context con , int id){
+		InventoryLineItemBookDB inventoryLineItemBookDB = new InventoryLineItemBookDB(con);
+		InventoryLineItem inventoryLineItem = inventoryLineItemBookDB.findByID(id);
+		inventoryLineItemBookDB.close();
+		return inventoryLineItem;
+	}
+	
+	public boolean remove(Context con , InventoryLineItem inventoryLineItem){
+		InventoryLineItemBookDB inventoryLineItemBookDB = new InventoryLineItemBookDB(con);
+		inventoryLineItemBookDB.delete(inventoryLineItem.getID());
+		inventoryLineItemBookDB.close();
 		return true;
-		 
-	 }
+	}
+	
+	public boolean remove(Context con , Item item){
+		db = new InventoryDB(con);
+		db.deleteItemByID(item.getID());
+		db.close();
+		return true;
+	}
+	
+	public boolean isContains(Context con , Item item){
+		Item i = getByID(con, item.getID());
+		return i != null;
+	}
+	
+	public int getQuantity(Context con){
+		InventoryDB inventoryDB = new InventoryDB(con);
+		int i = inventoryDB.findAll().size();
+		inventoryDB.close();
+		return i;
+	}
 }
--- src/com/core/database/ItemDescriptionBook.java
+++ src/com/core/database/ItemDescriptionBook.java
@@ -3,6 +3,7 @@ package com.core.database;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.R.string;
 import android.content.Context;
 
 import com.core.ItemDescription;
@@ -10,41 +11,53 @@ import com.database.ItemDescriptionBookDB;
 
 public class ItemDescriptionBook {
 	private ItemDescriptionBookDB db;
-	private List<ItemDescription> itemDescription;
-	public ItemDescriptionBook() {
-		itemDescription = new ArrayList<ItemDescription>();
-	}
 
-	public ItemDescription get(Context con,int id) {
+	public ItemDescription getByID(Context con,int id) {
 		db = new ItemDescriptionBookDB(con);
 		ItemDescription x = db.findBy(id);
 		db.close();
 		return x;
 	}
 
-	public void add(Context con,ItemDescription item) {
+	public ItemDescription add(Context con, String name , String description , float price , int barcode) {
+		ItemDescription itemDescription = new ItemDescription(0, name, description, price, barcode);
 		db = new ItemDescriptionBookDB(con);
-		db.insert(item);
+		itemDescription = db.insert(itemDescription);
 		db.close();
-		itemDescription.add(item);
+		return itemDescription;
 	}
 
-	public int getAmount(Context con) {
+	public ArrayList<ItemDescription> getAllItemDescriptions(Context con) {
 		db = new ItemDescriptionBookDB(con);
-		int x = db.findAll().length;
+		ArrayList<ItemDescription> x = db.findAll();
 		db.close();
 		return x;
 	}
 
-	public boolean remove(Context con,int id) {
+	public boolean remove(Context con,ItemDescription itemDescription) {
+		if(itemDescription.getItemDescription() == null || itemDescription.getName() == null) return false;
 		db = new ItemDescriptionBookDB(con);
-		db.delete(id);
+		db.deleteByID(itemDescription.getId());
 		db.close();
-		for (ItemDescription i : itemDescription) {
-			if (i.getId() == id)
-				return itemDescription.remove(i);
-		}
-		return false;
+		return true;
 	}
 
+	public boolean isContains(Context con , ItemDescription itemDescription){
+		ItemDescription i = getByID(con, itemDescription.getId());
+		return i != null;
+	}
+	
+	public int getQuantity(Context con){
+		db = new ItemDescriptionBookDB(con);
+		int i = db.findAll().size();
+		db.close();
+		return i;
+	}
+	
+	public ItemDescription getItemDescriptionByBarcode(Context con , int barcode){
+		db = new ItemDescriptionBookDB(con);
+		ItemDescription i = db.findByBarcode(barcode);
+		db.close();
+		return i;
+	}
 }
