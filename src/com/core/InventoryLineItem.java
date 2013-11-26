fa3e8a9f55a83bacd2a129dd8abfaf7d83ac51ec
nop <nop@ubuntu.ubuntu-domain>
2013-11-26 19:55:51 +0700

    Java Doc InventoryLineItem

--- src/com/core/InventoryLineItem.java
+++ src/com/core/InventoryLineItem.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * List of items in the inventory.
+ */
 public class InventoryLineItem implements Serializable{
 	 public static final String DATABASE_TABLE = "InventoryLineItemBook";
 	 public static final int DATABASE_VERSION = 1;
@@ -13,32 +16,61 @@ public class InventoryLineItem implements Serializable{
 	 
 	 public static final String COL_DATE = "date";
 	 
+	 /** The id of LineItem */
 	 private int id;
+	 /** The date of LineItem */
 	 private Date date;
+	 /** The list of items of LineItem */
 	 private List<Item> items = new ArrayList<Item>();
+	 
+	 /**
+	  * Creates a InventoryLineItem with initial id, list of items and date.
+	  * @param id the initial id of LineItem
+	  * @param items the initial list of items of LineItem
+	  * @param date the initial date of LineItem
+	  */
 	 public InventoryLineItem(int id,List<Item> items,Date date) {
 		this.id = id;
 		this.date = date;
 		this.items = items;
 	}
-	 
+	
+	 /**
+	  * Returns the date of LineItem.
+	  * @return date the date
+	  */
 	public Date getDate() {
 		return date;
 	}
 	
+	/**
+	 * Sets the date of LineItem.
+	 * @param date the date for setting
+	 */
 	public void setDate(Date date) {
 		this.date = date;
 	}
 	
+	/**
+	 * Returns the id of LineItem.
+	 * @return id the id*/
 	public int getID() {
 		return id;
 	}
 	
+	/**
+	 * Returns the list of item in LineItem
+	 * @return items the list of items
+	 * */
 	public List<Item> getItems()
 	{
 		return this.items;
 	}
 	
+	/**
+	 * Sets the list of items in LineItem.
+	 * @param items the list of items for setting
+	 */
 	public void setItems(List<Item> items) {
 		this.items = items;
 		
