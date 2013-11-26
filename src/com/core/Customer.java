3fae96b4ac8fa33c51909de86ce52bf3d161f39c
nop <nop@ubuntu.ubuntu-domain>
2013-11-26 19:45:10 +0700

    Java Doc Customer

--- src/com/core/Customer.java
+++ src/com/core/Customer.java
@@ -3,6 +3,9 @@ package com.core;
 import java.io.Serializable;
 import java.util.Date;
 
+/**
+ * The customer who buy product.
+ * */
 public class Customer implements Serializable {
     public static final String DATABASE_TABLE = "CustomerBook";
     public static final int DATABASE_VERSION = 1;
@@ -13,11 +16,22 @@ public class Customer implements Serializable {
     public static final String COL_EMAIL = "email";
     public static final String COL_DATE = "registerDate";
 	
+    /** The id of Customer */
 	private int id=0;
+	/** The name of Customer */
 	private String name="none";
+	/** The register date of Customer */
 	private Date registerDate;
+	/** The email of Customer */
 	private String email;
 	
+	/**
+	 * Creates a Customer with initial id, name, register date and email.
+	 * @param id the initial id of Customer
+	 * @param name the initial name of Customer
+	 * @param regsterDate the register date of Customer
+	 * @param email the initial email of Customer
+	 * */
 	public Customer(int id,String name,Date registerDate, String email) {
 		this.id = id;
 		this.name = name;
@@ -25,29 +39,53 @@ public class Customer implements Serializable {
 		this.email = email;
 	}
 	
+	/**
+	 * Returns the id of Customer.
+	 * @return id the id
+	 */
 	public int getID(){
 		return id;
 	}
 	
+	/**
+	 * Returns the name of Customer.
+	 * @return name the name
+	 */
 	public String getName()
 	{
 		return this.name;
 	}
 	
+	/**
+	 * Sets the name of Customer.
+	 * @param name the name for setting
+	 */
 	public void setName(String name)
 	{
 		this.name = name;
 	}
 	
+	/**
+	 * Returns the date of register in form of Date object.
+	 * @return registerDate the date of register
+	 */
 	public Date getRegisterDate()
 	{
 		return registerDate;
 	}
 
+	/**
+	 * Returns the email of Customer.
+	 * @return email the email
+	 */
 	public String getEmail() {
 		return email;
 	}
 
+	/**
+	 * Sets the email of Customer.
+	 * @param email the email for setting
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
