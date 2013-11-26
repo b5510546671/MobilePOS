cca217f53b1a08e5b3c8adc31b21cc836180fa3c
Sakarik <macintosh@kuwinwpa-138-82.kuwin.ku.ac.th>
2013-11-26 21:26:26 +0700

    New Java doc for the PaymentBook in core database!!

--- src/com/core/database/PaymentBook.java
+++ src/com/core/database/PaymentBook.java
@@ -6,10 +6,21 @@ import android.content.Context;
 
 import com.core.Payment;
 import com.database.PaymentBookDB;
-
+/**
+ * Access to the payment database which in the PaymentBookDB
+ */
 public class PaymentBook {
+	/**
+	 * PaymentBookDB is the payment in the database
+	 */
 	PaymentBookDB db;
 
+	/**
+	 * get the payment which relate to the input id
+	 * @param context is the context that received
+	 * @param id is the id of the payment
+	 * @return p is the payment that have the correct id
+	 */
 	public Payment getPaymentByID(Context context ,int id){
 		db = new PaymentBookDB(context);
 		Payment p = db.findByID(id);
@@ -17,6 +28,11 @@ public class PaymentBook {
 		return p;
 	}
 	
+	/**
+	 * get all the payment in the database
+	 * @param con is the context
+	 * @return p is the list of all payments in the database
+	 */
 	public List<Payment> getAllPayments(Context con){
 		db = new PaymentBookDB(con);
 		List<Payment> p = db.findAll();
@@ -24,6 +40,11 @@ public class PaymentBook {
 		return p;
 	}
 	
+	/**
+	 * get the number of payment in database
+	 * @param con is the context
+	 * @return the number of payments that contain in the database
+	 */
 	public int getQuantity(Context con){
 		return getAllPayments(con).size();
 	}
