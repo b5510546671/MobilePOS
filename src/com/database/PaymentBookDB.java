3ce338c074dfd3e86005d2afa333b993be5cfe54
benzintosh <benz_suankularb@hotmail.com>
2013-12-11 01:50:13 +0700

    javadoc

--- src/com/database/PaymentBookDB.java
+++ src/com/database/PaymentBookDB.java
@@ -10,12 +10,23 @@ import android.database.Cursor;
 import com.core.Customer;
 import com.core.Payment;
 
+/**
+* Payment DataAccessObject for SQLite.
+* @author Krittayot Techasombooranakit 5510545976
+*/
 public class PaymentBookDB extends GenericDao implements PaymentBookDao {
 
+	/**
+	 * Constructor of database.
+	 * @param context of application.
+	 */
 	public PaymentBookDB(Context ctx) {
 		super(ctx, GenericDao.dName, Payment.TABLE_CREATE, Payment.DATABASE_TABLE, Payment.DATABASE_VERSION);
 	}
 
+	/**
+	 * @see com.database.PaymentBookDao#insert(com.core.Payment)
+	 */
 	@Override
 	public Payment insert(Payment payment) {
 		ContentValues cv = new ContentValues();
@@ -24,6 +35,9 @@ public class PaymentBookDB extends GenericDao implements PaymentBookDao {
 		return new Payment((int)super.insert(Payment.DATABASE_TABLE , cv) , payment.getInput() , payment.getPrice());
 	}
 
+	/**
+	 * @see com.database.PaymentBookDao#findByID(int)
+	 */
 	@Override
 	public Payment findByID(int id) {
 		String[] columns = new String[]{GenericDao.KEY_ID , Payment.COL_INPUT_MONEY , Payment.COL_PRICE};
@@ -39,11 +53,17 @@ public class PaymentBookDB extends GenericDao implements PaymentBookDao {
 		return payment;
 	}
 
+	/**
+	 * @see com.database.PaymentBookDao#deleteByID(int)
+	 */
 	@Override
 	public int deleteByID(int id) {
 		return (int)super.delete(Payment.DATABASE_TABLE, id);
 	}
 
+	/**
+	 * @see com.database.PaymentBookDao#update(com.core.Payment)
+	 */
 	@Override
 	public Payment update(Payment payment) {
 		ContentValues cv = new ContentValues();
@@ -53,6 +73,9 @@ public class PaymentBookDB extends GenericDao implements PaymentBookDao {
 		return payment;
 	}
 
+	/**
+	 * @see com.database.PaymentBookDao#findAll()
+	 */
 	@Override
 	public List<Payment> findAll() {
 		String[] columns = new String[]{GenericDao.KEY_ID , Payment.COL_INPUT_MONEY , Payment.COL_PRICE};
--- src/com/database/PaymentBookDao.java
+++ src/com/database/PaymentBookDao.java
@@ -4,10 +4,43 @@ import java.util.List;
 
 import com.core.Payment;
 
+/**
+* Standard Payment DataAccessObject interface for any database.
+* @author Krittayot Techasombooranakit 5510545976
+*/
 public interface PaymentBookDao {
+	
+	/**
+	 * Insert payment to database.
+	 * @param payment without id.
+	 * @return payment with id.
+	 */
 	public Payment insert(Payment payment);
+	
+	/**
+	 * Find payment by id.
+	 * @param id of {@link Payment}
+	 * @return {@link Payment}.
+	 */
 	public Payment findByID(int id);
+	
+	/**
+	 * Get All {@link Payment} in database.
+	 * @return List of {@link Payment}.
+	 */
 	public List<Payment> findAll();
+	
+	/**
+	 * Delete {@link Payment} by id.
+	 * @param id of {@link Payment}
+	 * @return id.
+	 */
 	public int deleteByID(int id);
+	
+	/**
+	 * Update {@link Payment} to database.
+	 * @param payment want to update (depends on id).
+	 * @return {@link Payment} updated.
+	 */
 	public Payment update(Payment payment);
 }
